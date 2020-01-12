3f823169172cd4289091563293a81c9e3b1bb106
Kolappan <lekshminarayanan.kolappan>
2020-01-12 15:39:46 +0530

    ssh-agent invoked

--- scripts/terraform-apply.sh
+++ scripts/terraform-apply.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd aws-terraform; terraform apply  -var 'key_name=terraform' -var 'public_key_path=/var/lib/jenkins/.ssh/id_rsa.pub' -auto-approve
+cd aws-terraform; 
+eval `ssh-agent`; ssh-add /var/lib/jenkins/.ssh/id_rsa
+#terraform apply  -var 'key_name=terraform' -var 'public_key_path=/var/lib/jenkins/.ssh/id_rsa.pub' -auto-approve
 exit 0 
