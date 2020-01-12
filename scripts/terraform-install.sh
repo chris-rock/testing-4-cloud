ab46ebbd45a3068f694134aa722320f8bcf14e0c
Kolappan <lekshminarayanan.kolappan>
2020-01-12 10:21:29 +0530

    terra init

--- scripts/terraform-install.sh
+++ scripts/terraform-install.sh
@@ -8,7 +8,7 @@ status=$?
 #nlookup status
 if [ $status -eq 0 ]; then
   echo "terraform  found"
-  terraform -v; ls -al 
+  terraform -v; ls -al ; terraform init
   exit 0 
   else 
   wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip
