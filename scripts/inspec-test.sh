74c0c011c163d86d05f25dc2f248d9dabb6ccfb1
Kolappan <lekshminarayanan.kolappan>
2020-01-12 15:34:08 +0530

    auto approval added

--- scripts/inspec-test.sh
+++ scripts/inspec-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 cd aws-terraform
 export AWS_REGION=eu-west-2
-pwd; inspec -v
-#inspec exec test/verify -t aws:// --chef-license accept-silent
+#pwd; inspec -v
+inspec exec test/verify -t aws:// --chef-license accept-silent
 
--- scripts/terraform-apply.sh
+++ scripts/terraform-apply.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd aws-terraform; terraform apply -var 'key_name=terraform' -var 'public_key_path=/var/lib/jenkins/.ssh/id_rsa.pub'
+cd aws-terraform; terraform apply  -var 'key_name=terraform' -var 'public_key_path=/var/lib/jenkins/.ssh/id_rsa.pub' -auto-approve
 exit 0 
