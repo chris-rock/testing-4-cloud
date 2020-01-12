6b2fe18218737432610948040f76a128bbd6d5a2
Kolappan <lekshminarayanan.kolappan>
2020-01-12 15:28:37 +0530

    fix

--- scripts/inspec-test.sh
+++ scripts/inspec-test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
-cd aws-terraform;
+cd aws-terraform
 export AWS_REGION=eu-west-2
-inspec exec test/verify -t aws:// --chef-license accept-silent
+pwd; inspec -v
+#inspec exec test/verify -t aws:// --chef-license accept-silent
 
